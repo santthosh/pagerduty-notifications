df22094c9094102de4fa86f25be99578f4e4edf6
Santthosh <santthosh@gmail.com>
2015-10-30 04:20:57 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * Appirio Inc
+ *
+ * Created by santthosh on 10/30/15.
+ */
+exports.handler = function(event, context) {
+    console.log('Starting PagerDuty notifier');
+
+    console.log(event);
+
+    context.succeed("Not implemented yet");
+};
